e17aff12c9c72c83bb463413e0db647b911d9ab2
intello123 <93282495+intello123@users.noreply.github.com>
2021-11-03 09:59:15 -0400

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,20 +2,15 @@ input.onButtonPressed(Button.A, function () {
     nb_de_personnes += 1
 })
 input.onButtonPressed(Button.AB, function () {
-    nb_de_personnes += 0
+    nb_de_personnes = 0
 })
 input.onButtonPressed(Button.B, function () {
     nb_de_personnes += -1
 })
 let nb_de_personnes = 0
+nb_de_personnes = 0
 basic.forever(function () {
-    basic.showNumber(nb_de_personnes)
-})
-basic.forever(function () {
-    if (nb_de_personnes == 20 || nb_de_personnes > 20) {
-        if (input.buttonIsPressed(Button.A)) {
-            nb_de_personnes = 20
-        }
+    if (nb_de_personnes >= 10) {
         basic.showLeds(`
             . . # . .
             . . # . .
@@ -30,6 +25,7 @@ basic.forever(function () {
             # # # # #
             # # . # #
             `)
+        basic.showNumber(nb_de_personnes)
     } else {
         basic.showNumber(nb_de_personnes)
     }
