84c1b327ea576d7af80baea8fcc1aac108bf0aef
intello123 <93282495+intello123@users.noreply.github.com>
2021-11-03 09:34:25 -0400

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,37 @@
+input.onButtonPressed(Button.A, function () {
+    nb_de_personnes += 1
+})
+input.onButtonPressed(Button.AB, function () {
+    nb_de_personnes = 0
+})
+input.onButtonPressed(Button.B, function () {
+    nb_de_personnes += -1
+})
+let nb_de_personnes = 0
+nb_de_personnes = 0
+basic.forever(function () {
+    basic.showNumber(nb_de_personnes)
+})
+basic.forever(function () {
+    if (nb_de_personnes == 20) {
+        if (input.buttonIsPressed(Button.A)) {
+            nb_de_personnes = 20
+        }
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            . . # . .
+            . . . . .
+            . . # . .
+            `)
+        basic.showLeds(`
+            # # . # #
+            # # . # #
+            # # . # #
+            # # # # #
+            # # . # #
+            `)
+    } else {
+        basic.showNumber(nb_de_personnes)
+    }
+})
